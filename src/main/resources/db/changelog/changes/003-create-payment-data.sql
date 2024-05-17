a40469665d001de3500ab25f85f67526c033747b
DenisZaviryuhin <denzava30@gmail.com>
2024-05-17 18:18:24 +0300

    Liquibase settings , some changes in columns

--- src/main/resources/db/changelog/changes/003-create-payment-data.sql
+++ src/main/resources/db/changelog/changes/003-create-payment-data.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS payment_data (
     user_details_id INT,
     card_number INTEGER NOT NULL,
     cvv INTEGER NOT NULL,
-    card_date DATE NOT NULL,
+    expiry_date DATE NOT NULL,
     FOREIGN KEY (user_details_id) REFERENCES user_details(id)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
--- src/main/resources/db/changelog/changes/004-create-personal-area.sql
+++ src/main/resources/db/changelog/changes/004-create-personal-area.sql
@@ -1,7 +1,7 @@
     CREATE TABLE IF NOT EXISTS personal_area (
-        delete_area BOOLEAN,
         user_id INT,
         user_details_id INT,
+        delete_area BOOLEAN,
         FOREIGN KEY (user_id) REFERENCES users(id),
         FOREIGN KEY (user_details_id) REFERENCES user_details(id)
         ON UPDATE CASCADE ON DELETE CASCADE
