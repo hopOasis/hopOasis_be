2a2e642db43bb9b47ca1520da80864fbce44438e
Anna Padun <roananik1988@gmail.com>
2024-05-17 12:16:13 +0200

    create models

---
+++ src/main/java/com/example/hop_oasis/model/User.java
@@ -0,0 +1,35 @@
+package com.example.hop_oasis.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @OneToOne(mappedBy = "user")
+    UserProfile userProfile;
+
+
+
+
+}
