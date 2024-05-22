e4e9175d022b57f4a71adcfe6077e38f466eecc0
Anna Padun <roananik1988@gmail.com>
2024-05-22 15:12:12 +0200

    add Builder annotation

--- src/main/java/com/example/hop_oasis/dto/UserDto.java
+++ src/main/java/com/example/hop_oasis/dto/UserDto.java
@@ -2,15 +2,13 @@ package com.example.hop_oasis.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
     private Long id;
     @NotBlank(message = "Будь ласка, заповніть поле")
--- src/main/resources/db/changelog/changes/005-create-beer.sql
+++ src/main/resources/db/changelog/changes/005-create-beer.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS beer (
+CREATE TABLE IF NOT EXISTS beer(
     id INT AUTO_INCREMENT PRIMARY KEY,
     beer_name VARCHAR(100) NOT NULL,
     volume_large DECIMAL(7, 2) NOT NULL,
@@ -6,9 +6,8 @@ CREATE TABLE IF NOT EXISTS beer (
     price_large DECIMAL(7, 2) NOT NULL,
     price_small DECIMAL(7, 2) NOT NULL,
     description VARCHAR(500) NOT NULL,
-    bear_color VARCHAR(100) NOT NULL,
-    image VARCHAR(100) NOT NULL
-);
+    beer_color VARCHAR(100) NOT NULL
+    );
 
 
 
