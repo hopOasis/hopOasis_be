3058c743529d09696fbf33da2e7c20c24dc695cb
ihor <igork23@gmail.com>
2024-11-06 22:45:33 +0200

    restored ImageServiceImplTest

--- src/test/java/com/example/hop_oasis/service/data/ImageServiceImplTest.java
+++ src/test/java/com/example/hop_oasis/service/data/ImageServiceImplTest.java
@@ -1,152 +1,113 @@
-//package com.example.hop_oasis.service.data;
-//
-//import com.example.hop_oasis.convertor.ImageMapper;
-//import com.example.hop_oasis.decoder.ImageCompressor;
-//import com.example.hop_oasis.dto.ImageDto;
-//import com.example.hop_oasis.hendler.exception.ResourceNotFoundException;
-//import com.example.hop_oasis.model.Beer;
-//import com.example.hop_oasis.model.Image;
-//import com.example.hop_oasis.repository.BeerRepository;
-//import com.example.hop_oasis.repository.ImageRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.IOException;
-//import java.util.Optional;
-//
-//import static com.example.hop_oasis.hendler.exception.message.ExceptionMessage.*;
-//import static org.mockito.Mockito.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//class ImageServiceImplTest {
-//    private static final String IMAGE_NAME = "image.jpg";
-//    private static final Long ID = 1L;
-//    private static final Long BEER_ID = 1L;
-//    private static final byte[] COMPRESSED_IMAGE_DATA = new byte[]{/* Compressed data*/};
-//    private static final byte[] DECOMPRESSED_IMAGE_DATA = new byte[]{/* Decompressed data*/};
-//    @InjectMocks
-//    private ImageServiceImpl imageService;
-//    @Mock
-//    private ImageRepository imageRepository;
-//    @Mock
-//    private ImageCompressor imageCompressor;
-//    @Mock
-//    private ImageMapper imageMapper;
-//    @Mock
-//    private BeerRepository beerRepository;
-//    @Mock
-//    private Beer beer;
-//    @Mock
-//    private Image image;
-//    @Mock
-//    private MockMultipartFile multipartFile;
-//    private ImageDto imageDto;
-//    @BeforeEach
-//    void init() {
-//        image = new Image();
-//        image.setName(IMAGE_NAME);
-//        image.setImage(COMPRESSED_IMAGE_DATA);
-//
-//        imageDto = new ImageDto();}
-//    @Test
-//    void shouldReturnImageByName() {
-//        when(imageRepository.findFirstByName(IMAGE_NAME)).thenReturn(Optional.of(image));
-//        when(imageCompressor.decompressImage(COMPRESSED_IMAGE_DATA, IMAGE_NAME))
-//                .thenReturn(DECOMPRESSED_IMAGE_DATA);
-//        when(imageMapper.toDto(image)).thenReturn(imageDto);
-//
-//        ImageDto result = imageService.getImageByName(IMAGE_NAME);
-//
-//        assertNotNull(result);
-//        assertEquals(imageDto, result);
-//        verify(imageRepository).findFirstByName(IMAGE_NAME);
-//        verify(imageCompressor).decompressImage(COMPRESSED_IMAGE_DATA, IMAGE_NAME);
-//        assertArrayEquals(DECOMPRESSED_IMAGE_DATA, image.getImage());
-//        verify(imageMapper).toDto(image);
-//    }
-//    @Test
-//    void shouldThrowImageFoundException() {
-//        when(imageRepository.findFirstByName(IMAGE_NAME)).thenReturn(Optional.empty());
-//
-//        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
-//            imageService.getImageByName(IMAGE_NAME);
-//        });
-//
-//        String expectedMessage = String.format(RESOURCE_NOT_FOUND, IMAGE_NAME);
-//        assertEquals(expectedMessage, exception.getMessage());
-//        verify(imageRepository).findFirstByName(IMAGE_NAME);
-//        verify(imageCompressor, never()).decompressImage(any(byte[].class), eq(IMAGE_NAME));
-//        verify(imageMapper, never()).toDto(any(Image.class));
-//    }
-//    @Test
-//    void shouldAddImageToBeer() throws IOException {
-//       MultipartFile multipart = new MockMultipartFile(
-//                "file",
-//                IMAGE_NAME,
-//                "image/jpeg",
-//                COMPRESSED_IMAGE_DATA);
-//        when(imageCompressor.compressImage(multipart.getBytes())).thenReturn(COMPRESSED_IMAGE_DATA);
-//        when(beerRepository.findById(ID)).thenReturn(Optional.of(beer));
-//
-//        imageService.addImageToBeer(ID, multipart);
-//
-//        verify(imageCompressor, times(1)).compressImage(multipart.getBytes());
-//        verify(beerRepository, times(1)).findById(ID);
-//
-//        ArgumentCaptor<Image> argumentCaptor = ArgumentCaptor.forClass(Image.class);
-//        verify(imageRepository, times(1)).save(argumentCaptor.capture());
-//
-//        Image savedImage = argumentCaptor.getValue();
-//        assertEquals(IMAGE_NAME, savedImage.getName());
-//        assertEquals(beer, savedImage.getBeer());
-//        assertEquals(COMPRESSED_IMAGE_DATA, savedImage.getImage());
-//    }
-//    @Test
-//    void shouldThrowImageToCompressException() throws IOException {
-//        when(multipartFile.getBytes()).thenThrow(new IOException());
-//
-//        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
-//            imageService.addImageToBeer(ID, multipartFile);
-//        });
-//        String expectedMessage = String.format(RESOURCE_NOT_FOUND, "");
-//        assertEquals(expectedMessage, exception.getMessage());
-//        verify(multipartFile).getBytes();
-//        verify(imageCompressor, never()).compressImage(any(byte[].class));
-//        verify(beerRepository, never()).findById(anyLong());
-//        verify(imageRepository, never()).save(any(Image.class));
-//    }
-//    @Test
-//    void shouldThrowBeerNotFoundException() throws IOException {
-//        byte[] fileBytes = new byte[0];
-//        when(multipartFile.getBytes()).thenReturn(fileBytes);
-//        when(imageCompressor.compressImage(fileBytes)).thenReturn(COMPRESSED_IMAGE_DATA);
-//        when(multipartFile.getOriginalFilename()).thenReturn(IMAGE_NAME);
-//        when(beerRepository.findById(BEER_ID)).thenReturn(Optional.empty());
-//
-//        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () -> {
-//            imageService.addImageToBeer(BEER_ID, multipartFile);
-//        });
-//
-//        assertEquals(String.format(RESOURCE_NOT_FOUND, BEER_ID), exception.getMessage());
-//        verify(beerRepository).findById(BEER_ID);
-//        verify(imageRepository, never()).save(any(Image.class));
-//    }
-//    @Test
-//    void shouldDeleteImage() {
-//        image.setName(IMAGE_NAME);
-//
-//        when(imageRepository.findFirstByName(IMAGE_NAME)).thenReturn(Optional.of(image));
-//
-//        imageService.deleteImage(IMAGE_NAME);
-//
-//        verify(imageRepository, times(1)).delete(image);
-//    }
-//}
+package com.example.hop_oasis.service.data;
+
+import com.example.hop_oasis.convertor.BeerInfoMapper;
+import com.example.hop_oasis.dto.BeerInfoDto;
+import com.example.hop_oasis.handler.exception.ResourceNotFoundException;
+import com.example.hop_oasis.model.Beer;
+import com.example.hop_oasis.model.Image;
+import com.example.hop_oasis.repository.BeerRepository;
+import com.example.hop_oasis.repository.ImageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.Optional;
+
+import static com.example.hop_oasis.handler.exception.message.ExceptionMessage.RESOURCE_NOT_FOUND;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ImageServiceImplTest {
+    private static final String IMAGE_NAME = "image.jpg";
+    private static final Long ID = 1L;
+    private static final Long BEER_ID = 1L;
+    private static final byte[] COMPRESSED_IMAGE_DATA = new byte[]{/* Compressed data*/};
+
+    @Mock
+    private ImageRepository imageRepository;
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    @Mock
+    private S3Service s3Service;
+
+    @Mock
+    private BeerInfoMapper beerInfoMapper;
+
+    @Mock
+    private MockMultipartFile multipartFile;
+
+    @InjectMocks
+    private ImageServiceImpl imageService;
+
+    @Mock
+    private Beer beer;
+
+    @Mock
+    private Image image;
+
+    @BeforeEach
+    void setUp() {
+        image = new Image();
+        image.setName(IMAGE_NAME);
+    }
+
+    @Test
+    void shouldAddImageToBeer() throws MalformedURLException {
+        MultipartFile multipart = new MockMultipartFile(
+                "file",
+                IMAGE_NAME,
+                "image/jpeg",
+                COMPRESSED_IMAGE_DATA);
+        final var imageUrl = "http://server/beers/" + IMAGE_NAME;
+        when(beerRepository.findById(ID)).thenReturn(Optional.of(beer));
+        when(s3Service.getFileUrl(anyString())).thenReturn(URI.create(imageUrl).toURL());
+        when(beerInfoMapper.toDto(any(Beer.class))).thenReturn(any(BeerInfoDto.class));
+
+        imageService.addImageToBeer(ID, multipart);
+
+        verify(beerRepository, times(2)).findById(ID);
+
+        ArgumentCaptor<Image> argumentCaptor = ArgumentCaptor.forClass(Image.class);
+        verify(imageRepository, times(1)).save(argumentCaptor.capture());
+
+        Image savedImage = argumentCaptor.getValue();
+        assertEquals(imageUrl, savedImage.getName());
+        assertEquals(beer, savedImage.getBeer());
+    }
+
+
+    @Test
+    void shouldThrowBeerNotFoundException() {
+        when(beerRepository.findById(BEER_ID)).thenReturn(Optional.empty());
+
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class,
+                () -> imageService.addImageToBeer(BEER_ID, multipartFile));
+
+        assertEquals(String.format(RESOURCE_NOT_FOUND, BEER_ID), exception.getMessage());
+        verify(beerRepository).findById(BEER_ID);
+        verify(imageRepository, never()).save(any(Image.class));
+    }
+
+    @Test
+    void shouldDeleteImage() {
+        image.setName(IMAGE_NAME);
+
+        when(imageRepository.findFirstByName(IMAGE_NAME)).thenReturn(Optional.of(image));
+
+        imageService.deleteImage(IMAGE_NAME);
+
+        verify(imageRepository, times(1)).delete(image);
+    }
+}
