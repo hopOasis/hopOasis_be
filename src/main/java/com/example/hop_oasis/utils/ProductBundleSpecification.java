58e135af9c2b8509c2cd826e2fed641832b3fc21
IhorKulishenko <ihor.kulishenko@gmail.com>
2024-11-08 09:09:39 +0200

    fixed sorting by price

--- src/main/java/com/example/hop_oasis/utils/ProductBundleSpecification.java
+++ src/main/java/com/example/hop_oasis/utils/ProductBundleSpecification.java
@@ -1,7 +1,7 @@
 package com.example.hop_oasis.utils;
 
-import com.example.hop_oasis.model.CiderOptions;
 import com.example.hop_oasis.model.ProductBundle;
+import com.example.hop_oasis.model.ProductBundleOptions;
 import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.Subquery;
 import lombok.AccessLevel;
@@ -24,9 +24,9 @@ public class ProductBundleSpecification {
     private static Specification<ProductBundle> sortByPrice(String sortDirection) {
         return (root, query, criteriaBuilder) -> {
             Subquery<Double> subquery = query.subquery(Double.class);
-            Root<CiderOptions> ciderOptionsRoot = subquery.from(CiderOptions.class);
-            subquery.select(criteriaBuilder.max(ciderOptionsRoot.get("price")))
-                    .where(criteriaBuilder.equal(ciderOptionsRoot.get("productBundle"), root));
+            Root<ProductBundleOptions> pbOptionsRoot = subquery.from(ProductBundleOptions.class);
+            subquery.select(criteriaBuilder.max(pbOptionsRoot.get("price")))
+                    .where(criteriaBuilder.equal(pbOptionsRoot.get("productBundle"), root));
             if (sortDirection == null) {
                 query.orderBy(criteriaBuilder.asc(root.get("id")));
             } else if (sortDirection.equalsIgnoreCase("asc")) {
--- src/main/java/com/example/hop_oasis/utils/SnackSpecification.java
+++ src/main/java/com/example/hop_oasis/utils/SnackSpecification.java
@@ -1,7 +1,7 @@
 package com.example.hop_oasis.utils;
 
-import com.example.hop_oasis.model.CiderOptions;
 import com.example.hop_oasis.model.Snack;
+import com.example.hop_oasis.model.SnackOptions;
 import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.Subquery;
 import lombok.AccessLevel;
@@ -23,9 +23,9 @@ public class SnackSpecification {
     private static Specification<Snack> sortByPrice(String sortDirection) {
         return (root, query, criteriaBuilder) -> {
             Subquery<Double> subquery = query.subquery(Double.class);
-            Root<CiderOptions> ciderOptionsRoot = subquery.from(CiderOptions.class);
-            subquery.select(criteriaBuilder.max(ciderOptionsRoot.get("price")))
-                    .where(criteriaBuilder.equal(ciderOptionsRoot.get("snack"), root));
+            Root<SnackOptions> snacksOptionsRoot = subquery.from(SnackOptions.class);
+            subquery.select(criteriaBuilder.max(snacksOptionsRoot.get("price")))
+                    .where(criteriaBuilder.equal(snacksOptionsRoot.get("snack"), root));
             if (sortDirection == null) {
                 query.orderBy(criteriaBuilder.asc(root.get("id")));
             } else if (sortDirection.equalsIgnoreCase("asc")) {
