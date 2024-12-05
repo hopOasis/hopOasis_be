97e45634cf99892de0dba977312457c920853ad8
VasylMakar <vmakar@aviso.ca>
2024-12-05 02:07:54 +0200

    temp: add multiple ant matchers

--- src/main/java/com/example/hop_oasis/config/SecurityConfig.java
+++ src/main/java/com/example/hop_oasis/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.hop_oasis.config;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 import com.example.hop_oasis.filter.JwtAuthenticationFilter;
 import com.example.hop_oasis.service.data.UserDetailsServiceImpl;
 import java.util.Arrays;
@@ -17,7 +19,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -49,7 +50,12 @@ public class SecurityConfig {
         return http
             .csrf(AbstractHttpConfigurer::disable)
             .cors(AbstractHttpConfigurer::disable)
-            .authorizeRequests().requestMatchers(AntPathRequestMatcher.antMatcher("/health")).permitAll()
+            .authorizeRequests()
+            .requestMatchers(antMatcher("/health"),
+                             antMatcher("/auth/register"),
+                             antMatcher("/auth/login"),
+                             antMatcher("/images/**"))
+            .permitAll()
             .and()
             .userDetailsService(userDetailsService)
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
