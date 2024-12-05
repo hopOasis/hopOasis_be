11aac3078a4174231a32b68186a98aef66eaa3f1
VasylMakar <vmakar@aviso.ca>
2024-12-05 13:44:03 +0200

    register reflection classes in annotation

--- src/main/java/com/example/hop_oasis/HopOasisApplication.java
+++ src/main/java/com/example/hop_oasis/HopOasisApplication.java
@@ -2,10 +2,8 @@ package com.example.hop_oasis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(value = GraalVmRuntimeHints.class)
 public class HopOasisApplication {
 
     public static void main(String[] args) {
